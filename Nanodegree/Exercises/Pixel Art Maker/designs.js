66972c1d3b1af7ad87a10ff516f0d10ce8f00e0b
Omar Shatani <omar.shatani96@gmail.com>
2018-03-25 22:03:37 +0200

    Started makeGrid function

--- Nanodegree/Exercises/Pixel Art Maker/designs.js
+++ Nanodegree/Exercises/Pixel Art Maker/designs.js
@@ -1,16 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-//Variables declaration
-const sizePicker = document.getElementById('sizePicker');
-const colorPicker = document.getElementById('colorPicker');
-const inputWidth = document.getElementById('inputWidth');
-const inputHeight = document.getElementById('inputHeight');
-let width = inputWidth.value;
-let height = inputHeight.value;
-let color = colorPicker.value;
-const canvas = document.getElementById('pixelCanvas');
-const canvas__tbody = canvas.firstElementChild; 
+  //Variables declaration
+  const sizePicker = document.getElementById('sizePicker');
+  const colorPicker = document.getElementById('colorPicker');
+  const inputWidth = document.getElementById('inputWidth');
+  const inputHeight = document.getElementById('inputHeight');
+  let width = inputWidth.value;
+  let height = inputHeight.value;
+  let color = colorPicker.value;
+  const canvas = document.getElementById('pixelCanvas');
+  const canvas__tbody = canvas.firstElementChild;
 
+  //Create a grid WIDTH x HEIGHT
+  function makeGrid (width, height) {
+    canvas.removeChild(canvas__tbody);
+    for (let i = 0; i < width; i++) {
+      // const aBlock = document.createElement('tbody').appendChild(document.createElement('tr')).insertAdjacentElement('afterbegin', document.createElement('tr'));
+    }
+  }
 
 
 

619e15cb78c07c499ed337794ddd2a1357e0d7bf
Omar Shatani <omar.shatani96@gmail.com>
2018-03-25 21:45:39 +0200

    Added variables declaration

---
+++ Nanodegree/Exercises/Pixel Art Maker/designs.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+//Variables declaration
+const sizePicker = document.getElementById('sizePicker');
+const colorPicker = document.getElementById('colorPicker');
+const inputWidth = document.getElementById('inputWidth');
+const inputHeight = document.getElementById('inputHeight');
+let width = inputWidth.value;
+let height = inputHeight.value;
+let color = colorPicker.value;
+const canvas = document.getElementById('pixelCanvas');
+const canvas__tbody = canvas.firstElementChild; 
+
+
+
+
+
+
+
+
+
+
+
+});

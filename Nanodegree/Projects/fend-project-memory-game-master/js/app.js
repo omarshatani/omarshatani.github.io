1a296c7bedad6336b25a03d76eb85734929366e8
Omar Shatani <omar.shatani96@gmail.com>
2018-04-03 03:25:02 +0200

    Added stars logic and update

--- Nanodegree/Projects/fend-project-memory-game-master/js/app.js
+++ Nanodegree/Projects/fend-project-memory-game-master/js/app.js
@@ -5,70 +5,27 @@ document.addEventListener('DOMContentLoaded', function(event) {
   let shuffledCards;
   let clickStack = [];
   let winCounter = 0;
+  let limit = 14;
   const victory = document.querySelector('.victory');
   const moves = document.querySelector('.moves');
   const result = document.querySelector('.result');
   const restart = document.querySelector('.restart');
   const retry = document.querySelector('.retry');
+  const stars = document.querySelector('.stars');
+  const starsCount = document.querySelector('.starsCount');
   cards = Array.from(cards);
   shuffledCards = Array.from(cards);
-
-  // Game initialization
-  function init() {
-    // Resets win counter
-    winCounter = 0;
-    // Resets moves
-    moves.innerText = "0";
-    // Shuffling each card and update
-    deck.innerHTML = "";
-    shuffle(shuffledCards);
-    for (card of shuffledCards) {
-      deck.append(card);
-    }
-  }
-
-  //Game logic
-  function Game() {
-    if (clickStack.length > 1) {
-      // If cards match, set new class
-      if (compare(clickStack)) {
-        clickStack[0].setAttribute('class', 'card match');
-        clickStack[1].setAttribute('class', 'card match');
-        clickStack = [];
-        winCounter++;
-      } else {
-        // If cards don't match, close them and empty stack
-        setTimeout(function() {
-          close(clickStack[0]);
-          close(clickStack[1]);
-          clickStack = [];
-        }, 1000);
-        // Increment moves counter
-        moves.innerText++;
-      }
-    }
-    // Win check
-    if (winCounter === 8) {
-      // Open win interface
-      victory.style.display = "block";
-      // Update result
-      result.innerText = moves.innerText;
-    }
-  }
 
   // Closes a single card
   function close(card) {
     card.setAttribute('class', 'card');
   }
 
-  // Resets games
-  function reset() {
-    // Resets moves
-    moves.innerText = "0";
-    // Closes all cards
-    for (card of deck.children) {
-      // card.setAttribute('class', 'card');
-      close(card);
+  // Stars reset
+  function starsReset () {
+    stars.innerHTML = "";
+    for (let i = 0; i < 3; i++) {
+    stars.insertAdjacentHTML('afterbegin', '<li><i class="fa fa-star"></i></li>');
     }
   }
 
@@ -94,6 +51,70 @@ document.addEventListener('DOMContentLoaded', function(event) {
     return array;
   }
 
+  // Game initialization / reset
+  function init() {
+    //Stars initialization
+    starsReset();
+    // Resets win counter
+    winCounter = 0;
+    // Resets moves
+    moves.innerText = "0";
+    // Shuffling each card and update
+    deck.innerHTML = "";
+    shuffle(shuffledCards);
+    for (card of shuffledCards) {
+      deck.append(card);
+    }
+    // Closes all cards
+    for (card of deck.children) {
+      close(card);
+    }
+
+  }
+
+  //Game logic
+  function Game() {
+    if (clickStack.length > 1) {
+      // If cards match, set new class
+      if (compare(clickStack)) {
+        clickStack[0].setAttribute('class', 'card match');
+        clickStack[1].setAttribute('class', 'card match');
+        clickStack = [];
+        // Increment win counter
+        winCounter++;
+        // Increment moves counter
+        moves.innerText++;
+      } else {
+        // If cards don't match, close them and empty stack
+        setTimeout(function() {
+          close(clickStack[0]);
+          close(clickStack[1]);
+          clickStack = [];
+        }, 1000);
+      }
+      // Increment moves counter
+      moves.innerText++;
+    }
+
+    // Win check
+    if (winCounter === 8) {
+      // Open win interface
+      victory.style.display = "block";
+      // Update result
+      result.innerText = moves.innerText;
+      // Update stars result
+      starsCount.innerText = stars.childElementCount.toString();
+    }
+
+    // Stars logic
+    if (stars.childElementCount > 0) {
+      if (moves.innerText >= limit) {
+        stars.firstElementChild.remove();
+        limit += 9;
+        }
+    }
+  }
+
   // Listener for card click
   deck.addEventListener('click', function(event) {
     // Opens card if clicked and adds to stack
@@ -101,19 +122,20 @@ document.addEventListener('DOMContentLoaded', function(event) {
       clickStack.push(event.target);
       event.target.classList.add('open', 'show');
     }
+
     // Game logic update
     Game();
   });
 
+
   // Restart button listener
   restart.addEventListener('click', function() {
-    reset();
+    init();
   });
 
   // Play again listener
   retry.addEventListener('click', function() {
     init();
-    reset();
     victory.style.display = "none";
   });
 
